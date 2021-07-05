a2c340ffbf215c37e57c0a1eb553f6bfca11a616
DinossauroBebado <guipsilva32@gmail.com>
2021-07-05 20:40:42 -0300

    add difusão

--- 2LeiDeFick.py
+++ 2LeiDeFick.py
@@ -3,7 +3,7 @@ from sympy import *
 
 
 # segunda lei de fick
-def leiDeFick(variaveis, ingognita):
+def leiDeFick(variaveis, incognita):
     Cx, Czero, Cs, D, t, x = symbols("Cx, Czero,Cs,D,t,x")
 
     z = x/(2*sqrt(D*t))
@@ -14,19 +14,46 @@ def leiDeFick(variaveis, ingognita):
 
     equa = Eq(expr1, expr2)
 
-    res = solve(equa, ingognita)[0].subs(variaveis)
+    res = solve(equa, incognita)[0].subs(variaveis)
+
+    return(res)
+
+
+def difusao(variaveis, incognita):
+
+    Dzero, D, T, R, Q = symbols("Dzero,D, T, R, Q")
+
+    expr1 = Dzero*exp(-Q/(R*T))
+    expr2 = D
+
+    equa = Eq(expr1, expr2)
+
+    res = solve(equa, incognita)[0].subs(variaveis)
 
     return(res)
 
 
 Cx, Czero, Cs, D, t, x = symbols("Cx, Czero,Cs,D,t,x")
 
+Dzero, D, T, R, Q = symbols("Dzero,D, T, R, Q")
+
 variaveis = [
-    (Cx, 0.25),
-    (Czero, 0.55),
-    (Cs, 0),
-    (D, 4.3*10**-11),
-    (t, 36000)  # segundos
+    # (Cx, 0.3),
+    (Czero, 0),
+    (Cs, 0.2),
+    (D, 1.9*10**-11),
+    (t, 25*60*60),  # segundos
+    (x, 2*10**-3)
 ]
 
-print(N(leiDeFick(variaveis, x)))
+variaveisDifusao = [
+    (Dzero,  6.2*10**-7),
+    #(D, 6.2*10**-7),
+    (T, 600+273),  # kelvin
+    (R, 8.31),
+    (Q, 80*10**-3)]
+
+
+print((difusao(variaveisDifusao, D).evalf()))
+
+#print(N(leiDeFick(variaveis, Cx)))
